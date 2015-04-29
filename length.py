af622f9308b52523e4b7a33ca8dc2e9951d7ef73
flavio-casacurta <flavio@casacurta.com>
2015-04-28 21:49:36 -0300

    BKP diario

---
+++ length.py
@@ -0,0 +1,3 @@
+from calc_length import *
+cbl = open(r'C:\BNB\S303\ORIGINAIS\BOOK\B303001.txt').readlines()
+print (calc_length(cbl))
--- testCols.py
+++ testCols.py
@@ -1,8 +1,8 @@
 from columns import Columns
 col = Columns()
-cols = col.columns(r'C:\BNB\S303\ORIGINAIS\BOOKR\B303003.txt')
+cols = col.columns(r'C:\BNB\S303\ORIGINAIS\BOOK\B303001.txt')
 for c in cols:
     print c
-cols = col.columns(r'C:\BNB\S303\ORIGINAIS\BOOKR\B303003.txt', fmt='cob')
+cols = col.columns(r'C:\BNB\S303\ORIGINAIS\BOOK\B303001.txt', fmt='cob')
 for c in cols:
     print c
